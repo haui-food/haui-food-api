ec3845eab4c9f183ed808f0fa70298bac18b9c68
dngiang <dongocgiang.edu@gmail.com>
2024-03-17 14:11:25 +0700

    🌐 Config multi language (#1)

---
+++ src/messages/index.js
@@ -0,0 +1,1 @@
+module.exports.systemMessage = require('./system.message');
--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { env, logger, morgan } = require('./config');
+const { env, logger, morgan, i18nService } = require('./config');
 const { errorConverter, errorHandler } = require('./middlewares/error.middleware');
 const ApiError = require('./utils/ApiError');
 const httpStatus = require('http-status');
+const { systemMessage } = require('./messages');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  next(i18nService.setLocale(req, res));
+});
 
 if (env.nodeEnv !== 'test') {
   app.use(morgan.successHandler);
@@ -16,8 +24,13 @@ app.get('/', (req, res) => {
   res.send('Server HaUI Food is running 🎉');
 });
 
+app.get('/locales/:lang', (req, res) => {
+  res.cookie('lang', req.params.lang);
+  res.redirect('/');
+});
+
 app.all('*', (req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Resource not found.'));
+  next(new ApiError(httpStatus.NOT_FOUND, systemMessage().RESOURCE_NOT_FOUND));
 });
 
 app.use(errorConverter);
