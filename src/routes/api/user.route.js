9b76a0fccf887d942ba111483cb8af38d2a68d8b
dngiang <dongocgiang.edu@gmail.com>
2024-03-18 19:55:03 +0700

    🦺 Create middleware validation API CRUD user (#5)

--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const validate = require('../../middlewares/validate.middleware');
 const { userController } = require('../../controllers');
+const { userValidation } = require('../../validations');
 const userRouter = express.Router();
 
-userRouter.route('/').get(userController.getUsers).post(userController.createUser);
+userRouter
+  .route('/')
+  .get(validate(userValidation.getUsers), userController.getUsers)
+  .post(validate(userValidation.createUser), userController.createUser);
 
 userRouter
   .route('/:userId')
-  .get(userController.getUser)
-  .put(userController.updateUser)
-  .delete(userController.deleteUser)
-  .options(userController.lockUser);
+  .get(validate(userValidation.getUser), userController.getUser)
+  .put(validate(userValidation.updateUser), userController.updateUser)
+  .delete(validate(userValidation.deleteUser), userController.deleteUser)
+  .options(validate(userValidation.lockUser), userController.lockUser);
 
 module.exports = userRouter;
