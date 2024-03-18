22a92dfa6cd2f1d0fa099b5e3fe02a8110855d3c
VDucDung <dunga3qv2@gmail.com>
2024-03-18 23:44:42 +0700

    ✨ Create category model (#8)

---
+++ src/models/category.model.js
@@ -0,0 +1,20 @@
+const categorySchema = mongose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    images: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+module.exports = mongose.model('Category', categorySchema);
