8c4cf2abcbcf2aa13741bd1aada89748421b01a7
VDucDung <dunga3qv2@gmail.com>
2024-03-25 14:17:38 +0700

    👍 Implement message service (#41)

--- src/messages/index.js
+++ src/messages/index.js
@@ -4,3 +4,4 @@ module.exports.systemMessage = require('./system.message');
 module.exports.productMessage = require('./product.message');
 module.exports.contactMessage = require('./contact.message');
 module.exports.categoryMessage = require('./category.message');
+module.exports.messageMessage = require('./message.message');
---
+++ src/messages/message.message.js
@@ -0,0 +1,14 @@
+const { i18nService } = require('../config');
+
+const categoryMessage = () => {
+  return {
+    NOT_FOUND: i18nService.translate('message', 'notFound'),
+    FIND_SUCCESS: i18nService.translate('message', 'findSuccess'),
+    FIND_LIST_SUCCESS: i18nService.translate('message', 'findListSuccess'),
+    CREATE_SUCCESS: i18nService.translate('message', 'createSuccess'),
+    UPDATE_SUCCESS: i18nService.translate('message', 'updateSuccess'),
+    DELETE_SUCCESS: i18nService.translate('message', 'deleteSuccess'),
+  };
+};
+
+module.exports = categoryMessage;
