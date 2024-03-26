205357b2f760b90391aa531339f602da6a034cc2
VDucDung <dunga3qv2@gmail.com>
2024-03-26 23:10:56 +0700

    🌱 Handle API CRUD cart detail (#55)

---
+++ src/controllers/CartDetail.controller.js
@@ -0,0 +1,38 @@
+const httpStatus = require('http-status');
+const response = require('../utils/response');
+const catchAsync = require('../utils/catchAsync');
+const { cartDetailService } = require('../services');
+const { cartDetailMessage } = require('../messages');
+
+const createCartDetail = catchAsync(async (req, res) => {
+  const cartDetail = await cartDetailService.createCartDetail(req.body);
+  res.status(httpStatus.CREATED).json(response(httpStatus.CREATED, cartDetailMessage().CREATE_SUCCESS, cartDetail));
+});
+
+const getCartDetails = catchAsync(async (req, res) => {
+  const cartDetails = await cartDetailService.getCartDetailsByKeyword(req.query);
+  res.status(httpStatus.OK).json(response(httpStatus.OK, cartDetailMessage().FIND_LIST_SUCCESS, cartDetails));
+});
+
+const getCartDetailById = catchAsync(async (req, res) => {
+  const cartDetail = await cartDetailService.getCartDetailById(req.params.cartDetailId);
+  res.status(httpStatus.OK).json(response(httpStatus.OK, cartDetailMessage().FIND_SUCCESS, cartDetail));
+});
+
+const updateCartDetail = catchAsync(async (req, res) => {
+  const cartDetail = await cartDetailService.updateCartDetailById(req.params.cartDetailId, req.body);
+  res.status(httpStatus.OK).json(response(httpStatus.OK, cartDetailMessage().UPDATE_SUCCESS, cartDetail));
+});
+
+const deleteCartDetail = catchAsync(async (req, res) => {
+  const cartDetail = await cartDetailService.deleteCartDetailById(req.params.cartDetailId);
+  res.status(httpStatus.OK).json(response(httpStatus.OK, cartDetailMessage().DELETE_SUCCESS, cartDetail));
+});
+
+module.exports = {
+  createCartDetail,
+  getCartDetails,
+  getCartDetailById,
+  updateCartDetail,
+  deleteCartDetail,
+};
