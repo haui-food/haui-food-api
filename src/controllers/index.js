e58e913cf1aa747a713366f2140440c2d6e8e93f
VDucDung <dunga3qv2@gmail.com>
2024-03-27 16:27:29 +0700

    🌱 Handle API CRUD orders (#63)

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,3 +7,4 @@ module.exports.categoryController = require('./category.controller');
 module.exports.messageController = require('./message.controller');
 module.exports.cartDetailController = require('./cartDetail.controller');
 module.exports.cartController = require('./cart.controller');
+module.exports.orderController = require('./order.controller');
