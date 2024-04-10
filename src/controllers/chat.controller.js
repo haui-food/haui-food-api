7a224a54e1478b422f2d74d30c28c52430bacfd2
VDucDung <dunga3qv2@gmail.com>
2024-04-10 09:24:37 +0700

    🎨 Format code project

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,4 +1,5 @@
 const httpStatus = require('http-status');
+
 const response = require('../utils/response');
 const { chatService } = require('../services');
 const { messageMessage } = require('../messages');
