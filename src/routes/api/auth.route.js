843f1c7b98d9c6278cd35fb81aa20c3cbd998072
dngiang <dongocgiang.edu@gmail.com>
2024-04-18 13:57:44 +0700

    🚀 Add logger authen to route auth

--- src/routes/api/auth.route.js
+++ src/routes/api/auth.route.js
@@ -6,13 +6,20 @@ const { authValidation } = require('../../validations');
 const { auth } = require('../../middlewares/auth.middleware');
 const validate = require('../../middlewares/validate.middleware');
 const { rateLimitAuth } = require('../../middlewares/rate-limit.middleware');
+const { logAuthenticatedRequest } = require('../../middlewares/logger.middleware');
 
 const authRouter = express.Router();
 
 authRouter
   .route('/me')
-  .get(auth, authController.getMe)
-  .put(auth, uploadService.uploadImage.single('avatar'), validate(authValidation.updateMe), authController.updateMe);
+  .get(auth, logAuthenticatedRequest, authController.getMe)
+  .put(
+    auth,
+    logAuthenticatedRequest,
+    uploadService.uploadImage.single('avatar'),
+    validate(authValidation.updateMe),
+    authController.updateMe,
+  );
 
 authRouter.use(rateLimitAuth);
 
@@ -24,14 +31,20 @@ authRouter.route('/refresh-tokens').post(validate(authValidation.refreshToken), 
 
 authRouter.route('/change-password').post(auth, validate(authValidation.changePassword), authController.changePassword);
 
+authRouter.route('/login-with-2fa').post(validate(authValidation.loginWith2FA), authController.loginWith2FA);
+
 authRouter
   .route('/toggle-2fa')
   .post(auth, validate(authValidation.toggle2FA), authController.toggleTwoFactorAuthentication);
 
-authRouter.route('/login-with-2fa').post(validate(authValidation.loginWith2FA), authController.loginWith2FA);
+authRouter.post('/generate-2fa-secret', auth, logAuthenticatedRequest, authController.generate2FASecret);
 
-authRouter.post('/generate-2fa-secret', auth, authController.generate2FASecret);
-
-authRouter.post('/change-2fa-secret', auth, validate(authValidation.change2FASecret), authController.change2FASecret);
+authRouter.post(
+  '/change-2fa-secret',
+  auth,
+  logAuthenticatedRequest,
+  validate(authValidation.change2FASecret),
+  authController.change2FASecret,
+);
 
 module.exports = authRouter;
