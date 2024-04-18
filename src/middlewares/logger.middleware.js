e34ddec2413bf36229eb5f765988d008b67712ab
dngiang <dongocgiang.edu@gmail.com>
2024-04-18 15:10:41 +0700

    ✨ Create api send log

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,9 +1,9 @@
+const fs = require('fs');
 const path = require('path');
-const fs = require('fs').promises;
 
 const catchAsync = require('../utils/catchAsync');
 const getInfoClient = require('../utils/getInfoClient');
-const { LOG_DIR, LOG_FILENAME, REQUEST_USER_KEY } = require('../constants');
+const { LOG_DIR, LOG_FILENAME } = require('../constants');
 
 const pathFileLog = path.join(__dirname, '../../', LOG_DIR, LOG_FILENAME);
 
@@ -12,20 +12,22 @@ const getCurrentDate = () => {
   return currentDate;
 };
 
-const sendLogToFile = async (message) => {
-  await fs.appendFile(pathFileLog, message + '\n');
+const writeLogToFile = async (message) => {
+  fs.appendFileSync(pathFileLog, message + '\n');
 };
 
-const logUnauthenticatedRequest = catchAsync(async (req, res, next) => {
+const logUnauthenticatedRequest = catchAsync((req, res, next) => {
   const isAuth = req.headers?.authorization ? true : false;
 
-  if (isAuth) return next();
+  const isLogger = req.originalUrl.toLowerCase() === '/logs';
+
+  if (isAuth || isLogger) return next();
 
   const { userIP } = getInfoClient(req);
 
   const message = `${getCurrentDate()} - ${req.method} ${req.originalUrl} - Anonymous|${userIP}`;
 
-  await sendLogToFile(message);
+  writeLogToFile(message);
 
   next();
 });
