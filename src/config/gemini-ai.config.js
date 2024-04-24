8794deb3dc2d2feff5fb0e47da6e3651eb079797
VDucDung <dunga3qv2@gmail.com>
2024-04-25 00:29:01 +0700

    ✅ Update using gemini api for chat bot api (#143)

---
+++ src/config/gemini-ai.config.js
@@ -0,0 +1,21 @@
+const { env } = require("../config");
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const chatHistory = [];
+const genAI = new GoogleGenerativeAI(env.apiKey);
+
+const startChat = () => {
+  return genAI.getGenerativeModel({ model: "gemini-pro" }).startChat({
+    history: chatHistory,
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  });
+};
+
+const addToHistory = async (role, message) => {
+  chatHistory.push({ role: role, parts: [{ text: message }] });
+};
+
+module.exports = { startChat, addToHistory };
