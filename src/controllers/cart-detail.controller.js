f045954c794d5900c81192ce0ed3fc01fb8fbb9d
dngiang <dongocgiang.edu@gmail.com>
2024-04-28 21:30:41 +0700

    ♻️ Refactor cart-detail controller

--- src/controllers/cart-detail.controller.js
+++ src/controllers/cart-detail.controller.js
@@ -1,4 +1,5 @@
 const httpStatus = require('http-status');
+
 const response = require('../utils/response');
 const catchAsync = require('../utils/catchAsync');
 const { cartDetailService } = require('../services');
@@ -15,24 +16,33 @@ const getCartDetails = catchAsync(async (req, res) => {
 });
 
 const getCartDetailById = catchAsync(async (req, res) => {
-  const cartDetail = await cartDetailService.getCartDetailById(req.params.cartDetailId);
+  const { cartDetailId } = req.params;
+
+  const cartDetail = await cartDetailService.getCartDetailById(cartDetailId);
+
   res.status(httpStatus.OK).json(response(httpStatus.OK, cartDetailMessage().FIND_SUCCESS, cartDetail));
 });
 
 const updateCartDetail = catchAsync(async (req, res) => {
-  const cartDetail = await cartDetailService.updateCartDetailById(req.params.cartDetailId, req.body);
+  const { cartDetailId } = req.params;
+
+  const cartDetail = await cartDetailService.updateCartDetailById(cartDetailId, req.body);
+
   res.status(httpStatus.OK).json(response(httpStatus.OK, cartDetailMessage().UPDATE_SUCCESS, cartDetail));
 });
 
 const deleteCartDetail = catchAsync(async (req, res) => {
-  const cartDetail = await cartDetailService.deleteCartDetailById(req.params.cartDetailId);
+  const { cartDetailId } = req.params;
+
+  const cartDetail = await cartDetailService.deleteCartDetailById(cartDetailId);
+
   res.status(httpStatus.OK).json(response(httpStatus.OK, cartDetailMessage().DELETE_SUCCESS, cartDetail));
 });
 
 module.exports = {
-  createCartDetail,
   getCartDetails,
-  getCartDetailById,
+  createCartDetail,
   updateCartDetail,
   deleteCartDetail,
+  getCartDetailById,
 };
