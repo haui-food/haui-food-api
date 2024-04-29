321995f27c35e0d552e9f84e54854a5e13ba9c5a
dngiang <dongocgiang.edu@gmail.com>
2024-04-29 15:18:44 +0700

    ♻️ Refactor code auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -25,37 +25,48 @@ const {
 
 const login = async (email, password) => {
   const user = await userService.getUserByEmail(email);
+
   if (!user || !(await user.isPasswordMatch(password))) {
     throw new ApiError(httpStatus.UNAUTHORIZED, authMessage().INVALID_LOGIN);
   }
+
   if (!user.isVerify) {
     throw new ApiError(httpStatus.UNAUTHORIZED, authMessage().PLEASE_VERIFY_EMAIL);
   }
+
   if (user.isLocked) {
     throw new ApiError(httpStatus.LOCKED, userMessage().USER_LOCKED);
   }
+
+  const payload = { id: user.id, email: user.email };
+
   if (user.is2FA) {
-    const twoFaToken = generateToken(TOKEN_TYPES.TWO_FA, { id: user.id });
+    const twoFaToken = generateToken(TOKEN_TYPES.TWO_FA, payload);
     return { twoFaToken, user };
   }
-  const payload = { id: user.id };
-  const accessToken = generateToken(TOKEN_TYPES.ACCESS, payload);
-  const refreshToken = generateToken(TOKEN_TYPES.REFRESH, payload);
+
   user.lastActive = Date.now();
   await user.save();
   user.password = undefined;
+
+  const accessToken = generateToken(TOKEN_TYPES.ACCESS, payload);
+  const refreshToken = generateToken(TOKEN_TYPES.REFRESH, payload);
+
   return { user, accessToken, refreshToken };
 };
 
 const register = async (fullname, email, password) => {
   const expires = Date.now() + EXPIRES_TOKEN_EMAIL_VERIFY;
+
   const registerData = {
     fullname,
     email,
     password,
     verifyExpireAt: expires,
   };
+
   await userService.createUser(registerData);
+
   const tokenVerify = cryptoService.encryptObj(
     {
       email,
@@ -64,7 +75,9 @@ const register = async (fullname, email, password) => {
     },
     env.secret.tokenVerify,
   );
+
   const linkVerify = `${URL_HOST[env.nodeEnv]}/api/v1/auth/verify?token=${tokenVerify}`;
+
   await emailService.sendEmail({
     emailData: {
       emails: email,
@@ -76,72 +89,86 @@ const register = async (fullname, email, password) => {
 };
 
 const refreshToken = async (refreshToken) => {
-  let payload;
-  try {
-    payload = jwt.verify(refreshToken, env.jwt.secretRefresh);
-  } catch (err) {
-    throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
-  }
+  const payload = jwt.verify(refreshToken, env.jwt.secretRefresh);
+
   if (!payload || payload.type !== TOKEN_TYPES.REFRESH) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
-  const user = await userService.getUserById(payload.id);
+
+  const user = await userService.getUserByEmail(payload.email);
   if (!user) {
     throw new ApiError(httpStatus.UNAUTHORIZED, authMessage().INVALID_TOKEN);
   }
+
   if (user.isLocked) {
     throw new ApiError(httpStatus.UNAUTHORIZED, userMessage().USER_LOCKED);
   }
-  payload = { id: user.id };
-  const accessToken = generateToken(TOKEN_TYPES.ACCESS, payload);
+
+  data = { id: user.id, email: user.email };
+  const accessToken = generateToken(TOKEN_TYPES.ACCESS, data);
+
   return { accessToken };
 };
 
 const generateToken = (type, payload) => {
   const { secret, expiresIn } = tokenMappings[type];
+
   const token = jwt.sign({ ...payload, type }, secret, {
     expiresIn,
   });
+
   return token;
 };
 
 const changePassword = async (userId, oldPassword, newPassword) => {
   const user = await userService.getUserById(userId);
+
   if (!(await user.isPasswordMatch(oldPassword))) {
     throw new ApiError(httpStatus.UNAUTHORIZED, authMessage().INVALID_PASSWORD);
   }
+
   user.password = newPassword;
   await user.save();
   user.password = undefined;
+
   return user;
 };
 
 const toggleTwoFactorAuthentication = async (userId, code = '') => {
   const user = await userService.getUserById(userId);
+
   if (!user.is2FA && !(await user.is2FAMatch(code))) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_2FA_CODE);
   }
+
   user.is2FA = !user.is2FA;
   await user.save();
   user.password = undefined;
+
   return user;
 };
 
 const loginWith2FA = async (token2FA, code) => {
   const payload = jwt.verify(token2FA, env.jwt.secret2FA);
+
   if (!payload || payload.type !== TOKEN_TYPES.TWO_FA) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
+
   const user = await userService.getUserById(payload.id);
+
   if (!user || !(await user.is2FAMatch(code))) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_2FA_CODE);
   }
-  const payloadData = { id: user.id };
+
+  const payloadData = { id: user.id, email: user.email };
   const accessToken = generateToken(TOKEN_TYPES.ACCESS, payloadData);
   const refreshToken = generateToken(TOKEN_TYPES.REFRESH, payloadData);
+
   user.lastActive = Date.now();
   await user.save();
   user.password = undefined;
+
   return { user, accessToken, refreshToken };
 };
 
@@ -152,46 +179,61 @@ const generate2FASecret = () => {
 
 const verify2FA = (secret, code) => {
   const result = twoFactor.verifyToken(secret, code);
+
   if (!result) return false;
+
   return CODE_VERIFY_2FA_SUCCESS.includes(result.delta);
 };
 
 const change2FASecret = async (userId, secret, code) => {
   const user = await userService.getUserById(userId);
+
   const result = verify2FA(secret, code);
+
   if (!result) throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_2FA_CODE);
+
   user.secret = secret;
   await user.save();
   user.password = undefined;
+
   return user;
 };
 
 const verifyEmail = async (token) => {
   const { isExpired, payload } = cryptoService.expiresCheck(token, env.secret.tokenVerify);
+
   if (isExpired) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN_VERIFY_EXPIRED);
   }
+
   const user = await userService.getUserByEmail(payload.email);
+
   if (!user || user?.isVerify) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
+
   user.isVerify = true;
   user.verifyExpireAt = null;
   await user.save();
   user.password = undefined;
+
   return user;
 };
 
 const reSendEmailVerify = async (token) => {
   const expires = Date.now() + EXPIRES_TOKEN_EMAIL_VERIFY;
+
   const { isExpired, payload } = cryptoService.expiresCheck(token, env.secret.tokenVerify, TIME_DIFF_EMAIL_VERIFY);
+
   if (!isExpired) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().PLEASE_WAIT);
   }
+
   const user = await userService.getUserByEmail(payload.email);
   if (user.isVerify) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
+
   const tokenVerify = cryptoService.encryptObj(
     {
       expires,
@@ -200,6 +242,7 @@ const reSendEmailVerify = async (token) => {
     },
     env.secret.tokenVerify,
   );
+
   const linkVerify = `${URL_HOST[env.nodeEnv]}/api/v1/auth/verify?token=${tokenVerify}`;
   await emailService.sendEmail({
     emailData: {
@@ -209,25 +252,31 @@ const reSendEmailVerify = async (token) => {
     },
     type: EMAIL_TYPES.VERIFY,
   });
+
   user.verifyExpireAt = expires;
   await user.save();
 };
 
 const forgotPassword = async (email, text, sign) => {
   const isPassCaptcha = captchaService.verify(sign, text);
+
   if (!isPassCaptcha) {
     throw new ApiError(httpStatus.BAD_REQUEST, captchaMessage().INVALID_CAPTCHA);
   }
+
   const user = await userService.getUserByEmail(email);
   if (!user) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().EMAIL_NOT_EXISTS);
   }
+
   if (!user.isVerify) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().PLEASE_VERIFY_EMAIL);
   }
+
   const expires = Date.now() + EXPIRES_TOKEN_FOTGOT_PASSWORD;
   // const otp = generateOTP();
   const otp = '123456';
+
   const tokenForgot = cryptoService.encryptObj(
     {
       otp,
@@ -237,6 +286,7 @@ const forgotPassword = async (email, text, sign) => {
     },
     env.secret.tokenForgot,
   );
+
   // await emailService.sendEmail({
   //   emailData: {
   //     emails: email,
@@ -245,27 +295,36 @@ const forgotPassword = async (email, text, sign) => {
   //   },
   //   type: EMAIL_TYPES.FORGOT,
   // });
+
   user.forgotStatus = STATUS_FORGOT.VERIFY_OTP;
   await user.save();
+
   return tokenForgot;
 };
 
 const verifyOTPForgotPassword = async (tokenForgot, otp) => {
   const { isExpired, payload } = cryptoService.expiresCheck(tokenForgot, env.secret.tokenForgot);
+
   if (isExpired) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().TOKEN_EXPIRED);
   }
+
   if (payload.type != TOKEN_TYPES.FOTGOT) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
+
   if (payload.otp != otp) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_OTP);
   }
+
   const user = await userService.getUserByEmail(payload.email);
+
   if (!user || user?.forgotStatus !== STATUS_FORGOT.VERIFY_OTP) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
+
   const expires = Date.now() + EXPIRES_TOKEN_VERIFY_OTP_FORGOT;
+
   const tokenVerifyOTP = cryptoService.encryptObj(
     {
       expires,
@@ -274,25 +333,33 @@ const verifyOTPForgotPassword = async (tokenForgot, otp) => {
     },
     env.secret.tokenVerifyOTP,
   );
+
   user.forgotStatus = STATUS_FORGOT.VERIFIED;
   await user.save();
+
   return tokenVerifyOTP;
 };
 
 const resetPassword = async (tokenVerifyOTP, newPassword) => {
   const { isExpired, payload } = cryptoService.expiresCheck(tokenVerifyOTP, env.secret.tokenVerifyOTP);
+
   if (isExpired) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().TOKEN_EXPIRED);
   }
+
   if (payload.type != TOKEN_TYPES.VERIFY_OTP) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
+
   const user = await userService.getUserByEmail(payload.email);
+
   if (!user || user?.forgotStatus !== STATUS_FORGOT.VERIFIED) {
     throw new ApiError(httpStatus.BAD_REQUEST, authMessage().INVALID_TOKEN);
   }
+
   user.password = newPassword;
   user.forgotStatus = STATUS_FORGOT.DONE;
+
   await user.save();
 };
 
