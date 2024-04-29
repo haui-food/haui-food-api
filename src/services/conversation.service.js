045493b9f5a263a44af32117ed0595926836aa43
dngiang <dongocgiang.edu@gmail.com>
2024-04-29 15:43:55 +0700

    ♻️ Refactor code conversation service

--- src/services/conversation.service.js
+++ src/services/conversation.service.js
@@ -2,24 +2,29 @@ const httpStatus = require('http-status');
 
 const { Conversation } = require('../models');
 const ApiError = require('../utils/ApiError');
-const ApiFeature = require('../utils/ApiFeature');
 const { cartDetailMessage, conversationMessage } = require('../messages');
 
-const getConversationById = async (id) => {
-  const conversation = await Conversation.findById(id);
+const getConversationById = async (conversatioId) => {
+  const conversation = await Conversation.findById(conversatioId);
+
   if (!conversation) {
     throw new ApiError(httpStatus.NOT_FOUND, cartDetailMessage().NOT_FOUND);
   }
+
   return conversation;
 };
 
 const createConversation = async (conversationBody) => {
   const { participants } = conversationBody;
+
   const conversation = await Conversation.findOne({ participants });
+
   if (conversation) {
     throw new ApiError(httpStatus.BAD_REQUEST, conversationMessage().ALREADY_EXISTS);
   }
+
   const newConversation = await Conversation.create(conversationBody);
+
   return newConversation;
 };
 
@@ -30,21 +35,25 @@ const getConversationsByKeyword = async (query) => {
 
 const updateConversationById = async (conversationId, updateBody) => {
   const conversation = await getConversationById(conversationId);
+
   Object.assign(conversation, updateBody);
   await conversation.save();
+
   return conversation;
 };
 
 const deleteConversationById = async (conversationId) => {
   const conversation = await getConversationById(conversationId);
+
   await conversation.deleteOne();
+
   return conversation;
 };
 
 module.exports = {
-  getConversationById,
   createConversation,
-  getConversationsByKeyword,
+  getConversationById,
   updateConversationById,
   deleteConversationById,
+  getConversationsByKeyword,
 };
