69ff253091db6fa4f3633a4bc67a7ee7a5a4fba1
dngiang <dongocgiang.edu@gmail.com>
2024-05-05 11:40:47 +0700

    🚨 Disable service redis

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports.env = require('./env.config');
-module.exports.redis = require('./redis.config');
+// module.exports.redis = require('./redis.config');
 module.exports.logger = require('./logger.config');
 module.exports.morgan = require('./morgan.config');
 module.exports.chatAI = require('./gemini-ai.config');
