edc11928b834ef1b29866d86b013c077d8049a06
giang.dongoc <giang.dongoc@vti.com.vn>
2024-05-06 17:10:48 +0700

    ✨ Sort by length function

--- src/messages/shop.message.js
+++ src/messages/shop.message.js
@@ -3,9 +3,9 @@ const { i18nService } = require('../config');
 const shopMessage = () => {
   return {
     NOT_FOUND: i18nService.translate('shop.notFound'),
-    FIND_SUCCESS: i18nService.translate('shop.findSuccess'),
     SHOP_DETAIL: i18nService.translate('shop.shopDetail'),
     RESULT_FIND: i18nService.translate('shop.resultFind'),
+    FIND_SUCCESS: i18nService.translate('shop.findSuccess'),
   };
 };
 
