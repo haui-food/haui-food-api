076cd204a1e854ff238078e1a81b6dcb48b47e28
VDucDung <dunga3qv2@gmail.com>
2024-05-27 16:08:19 +0700

    ⚡️ Authentication required before using ai chatbot

--- src/routes/api/chat-bot.route.js
+++ src/routes/api/chat-bot.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const { chatBotConterller } = require('../../controllers');
 const { chatBotValidation } = require('../../validations');
+const { auth } = require('../../middlewares/auth.middleware');
 const validate = require('../../middlewares/validate.middleware');
 
 const chatBotRouter = express.Router();
 
-chatBotRouter.post('/', validate(chatBotValidation.sendMessage), chatBotConterller.chatBot);
+chatBotRouter.post('/', auth, validate(chatBotValidation.sendMessage), chatBotConterller.chatBot);
 
 module.exports = chatBotRouter;
