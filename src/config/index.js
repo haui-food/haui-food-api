81071e9d04b769d30ddbaf31da240bd90b978b2d
giang.dongoc <giang.dongoc@vti.com.vn>
2024-05-29 09:18:37 +0700

    ♻️ Refactor config rabbitmq

--- src/config/index.js
+++ src/config/index.js
@@ -5,3 +5,4 @@ module.exports.logger = require('./logger.config');
 module.exports.morgan = require('./morgan.config');
 module.exports.chatAI = require('./gemini-ai.config');
 module.exports.i18nService = require('./i18n.config');
+module.exports.rabbitmq = require('./rabbitmq.config');
