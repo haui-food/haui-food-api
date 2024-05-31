bb587c266dc39a387a6db5fbde81130b7cee5261
giang.dongoc <giang.dongoc@vti.com.vn>
2024-05-31 13:09:28 +0700

    ✨ Add message service order

--- src/messages/order.message.js
+++ src/messages/order.message.js
@@ -11,6 +11,16 @@ const categoryMessage = () => {
     FIND_LIST_SUCCESS: i18nService.translate('order.findListSuccess'),
     CANCEL_SUCCESS: i18nService.translate('order.cancelSuccess'),
     UPDATE_STATUS_SUCCESS: i18nService.translate('order.updateStatusSuccess'),
+    MAXIUM_ORDER: i18nService.translate('order.maximumOrder'),
+    ERROR_ORDER: i18nService.translate('order.errorOrder'),
+    LOW_BALANCE_ALERT: i18nService.translate('order.lowBalanceAlert'),
+    ORDER_UPDATE_FORBIDDEN: i18nService.translate('order.orderUpdateForbidden'),
+    CANCEL_ORDER_ERROR: i18nService.translate('order.cancelOrderError'),
+    REJECT_ORDER_ERROR: i18nService.translate('order.rejectOrderError'),
+    APPROVE_ORDER_ERROR: i18nService.translate('order.approveOrderError'),
+    UNABLE_TO_UPDATE_ORDER_STATUS: i18nService.translate('order.unableToUpdateOrderStatus'),
+    UNABLE_TO_COMPLETE_ORDER: i18nService.translate('order.unableToCompleteOrder'),
+    INVALID_ORDER_STATUS: i18nService.translate('order.invalidOrderStatus'),
   };
 };
 
