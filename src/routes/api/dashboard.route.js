d089ac7475f7cc3702e9eb633e1be884ac988304
VDucDung <dunga3qv2@gmail.com>
2024-06-02 23:35:08 +0700

    🔧 Update method statstical routes

--- src/routes/api/dashboard.route.js
+++ src/routes/api/dashboard.route.js
@@ -6,12 +6,12 @@ const { auth, authorize } = require('../../middlewares/auth.middleware');
 const chatRouter = express.Router();
 chatRouter.route('/statistical-user').get(auth, authorize(['admin']), dashboardController.statisticalUserByRole);
 
-chatRouter.route('/statistical-data').get(auth, authorize(['admin']), dashboardController.statisticalData);
+chatRouter.route('/statistical-data').post(auth, authorize(['admin']), dashboardController.statisticalData);
 
-chatRouter.route('/statistical-revenue').get(auth, authorize(['admin']), dashboardController.statisticalRevenue);
+chatRouter.route('/statistical-revenue').post(auth, authorize(['admin']), dashboardController.statisticalRevenue);
 
 chatRouter
   .route('/statistical-performance')
-  .get(auth, authorize(['admin']), dashboardController.statisticalPerformance);
+  .post(auth, authorize(['admin']), dashboardController.statisticalPerformance);
 
 module.exports = chatRouter;
