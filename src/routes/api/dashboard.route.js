e49458b53a5bcb5ba34e004afac3499510657889
VDucDung <dunga3qv2@gmail.com>
2024-06-02 23:52:59 +0700

    🔥 Fix conflict dashboard routes

--- src/routes/api/dashboard.route.js
+++ src/routes/api/dashboard.route.js
@@ -8,24 +8,14 @@ chatRouter
   .route('/statistical-user')
   .get(auth, authorize(['shop', 'admin']), dashboardController.statisticalUserByRole);
 
-<<<<<<< HEAD
-chatRouter.route('/statistical-data').post(auth, authorize(['admin']), dashboardController.statisticalData);
-
-chatRouter.route('/statistical-revenue').post(auth, authorize(['admin']), dashboardController.statisticalRevenue);
-
-chatRouter
-  .route('/statistical-performance')
-  .post(auth, authorize(['admin']), dashboardController.statisticalPerformance);
-=======
-chatRouter.route('/statistical-data').get(auth, authorize(['shop', 'admin']), dashboardController.statisticalData);
+chatRouter.route('/statistical-data').post(auth, authorize(['shop', 'admin']), dashboardController.statisticalData);
 
 chatRouter
   .route('/statistical-revenue')
-  .get(auth, authorize(['shop', 'admin']), dashboardController.statisticalRevenue);
+  .post(auth, authorize(['shop', 'admin']), dashboardController.statisticalRevenue);
 
 chatRouter
   .route('/statistical-performance')
-  .get(auth, authorize(['shop', 'admin']), dashboardController.statisticalPerformance);
->>>>>>> cf864f20bc4811a7c3a5835de4d9974c0e3a85dc
+  .post(auth, authorize(['shop', 'admin']), dashboardController.statisticalPerformance);
 
 module.exports = chatRouter;
