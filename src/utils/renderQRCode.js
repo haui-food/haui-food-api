aeb0dc491ed32aa27f95363bdbca61be267be75b
dngiang <dongocgiang.edu@gmail.com>
2024-11-23 20:35:37 +0700

    🐛 Hot fix generate QR code

--- src/utils/renderQRCode.js
+++ src/utils/renderQRCode.js
@@ -1,40 +1,16 @@
-const path = require('path');
-const { QRCodeCanvas } = require('@loskir/styled-qr-code-node');
-
-const {
-  LOGO_QRCODE_NAME,
-  LOGO_SIZE,
-  HOST_NAME,
-  LOGO_BACKGROUND_COLOR,
-  LOGO_DOTS_COLOR,
-  LOGO_MARGIN,
-  LOGO_IMAGE_MARGIN,
-} = require('../constants');
+const QRCode = require('qrcode');
 
 const options = {
-  width: LOGO_SIZE,
-  height: LOGO_SIZE,
-  margin: LOGO_MARGIN,
-  image: path.join(__dirname, '../..', 'public', 'images', LOGO_QRCODE_NAME),
-  dotsOptions: {
-    color: LOGO_DOTS_COLOR,
-    type: 'square',
-  },
-  backgroundOptions: {
-    color: LOGO_BACKGROUND_COLOR,
-  },
-  imageOptions: {
-    crossOrigin: HOST_NAME,
-    margin: LOGO_IMAGE_MARGIN,
-  },
+  margin: 2,
+  width: 300,
+  height: 300,
+  quality: 0.3,
+  type: 'image/jpeg',
+  errorCorrectionLevel: 'H',
 };
 
 const renderQRCode = async (url) => {
-  const qrCode = new QRCodeCanvas({ ...options, data: url });
-
-  const buffer = await qrCode.toBuffer('png');
-
-  return buffer;
+  return QRCode.toBuffer(url, options);
 };
 
 module.exports = renderQRCode;
